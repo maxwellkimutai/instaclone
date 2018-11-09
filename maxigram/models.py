794f44ba41846c261a699a8d4170516d9f07ce36
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-09 13:04:17 +0300

    add user relationships for profile and image models

--- maxigram/models.py
+++ maxigram/models.py
@@ -5,11 +5,21 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to = 'profile_images',blank = True)
     bio = models.TextField(max_length = 100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/',blank=True)
     image_name = models.CharField(max_length = 30)
     image_caption = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
     likes = models.IntegerField()
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.image_name
+
+    @classmethod
+    def get_images(cls):
+        images = cls.objects.all()
+        return images
