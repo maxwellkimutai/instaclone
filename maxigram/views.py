021cf308f00f4fd5e15b25d9da942a2d95028419
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-09 13:03:02 +0300

    add profile template view

--- maxigram/views.py
+++ maxigram/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import Image,Profile
+from django.contrib.auth.models import User
 
 # Create your views here.
+@login_required(login_url='accounts/login/')
 def index(request):
-    return render(request,"index.html")
+    images = Image.get_images()
+    return render(request,"index.html",{"images":images})
+
+@login_required(login_url='accounts/login/')
+def profile(request,username):
+    try:
+        user = User.objects.get(username = username)
+        profile = Profile.objects.get(user = user)
+        images = Image.objects.filter(profile = profile)
+        print(len(images))
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"profile.html",{"profile":profile, "images":images})
