108d4f8a5f53258caf8c9143fdedce793b3e6832
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-10 09:27:55 +0300

    add new image form

---
+++ maxigram/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Image
+
+class NewImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['user','profile','likes','pub_date']
