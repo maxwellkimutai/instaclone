1f7df8ad7fcdb6eb0377d8b538394939f8e931b1
“maxwellkimutai” <maxwellkimutai@gmail.com>
2019-01-21 12:31:54 +0300

    production

--- instagram/settings.py
+++ instagram/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 import dj_database_url
 from decouple import config,Csv
 
-MODE=config("MODE",default="dev")
+MODE=config("MODE",default="prod")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
